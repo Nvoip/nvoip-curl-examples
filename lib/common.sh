2e32d67375f164d959d934a9bca0bb134d643daf
Leandro Campos <40430440+leandronvoip@users.noreply.github.com>
2026-04-29 18:04:51 -0300

    Add initial cURL examples kit

---
+++ lib/common.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+
+NVOIP_BASE_URL="${NVOIP_BASE_URL:-https://api.nvoip.com.br/v2}"
+
+nvoip_require_var() {
+  var_name="$1"
+  eval "var_value=\${$var_name:-}"
+  if [ -z "$var_value" ]; then
+    printf 'Missing required variable: %s\n' "$var_name" >&2
+    return 1
+  fi
+}
+
+nvoip_encode_basic_auth() {
+  printf '%s' "$1:$2" | base64 | tr -d '\n'
+}
+
+nvoip_resolve_basic_auth() {
+  if [ -n "${NVOIP_OAUTH_BASIC_AUTH:-}" ]; then
+    printf '%s\n' "$NVOIP_OAUTH_BASIC_AUTH"
+    return 0
+  fi
+
+  nvoip_require_var NVOIP_OAUTH_CLIENT_ID || return 1
+  nvoip_require_var NVOIP_OAUTH_CLIENT_SECRET || return 1
+  nvoip_encode_basic_auth "$NVOIP_OAUTH_CLIENT_ID" "$NVOIP_OAUTH_CLIENT_SECRET"
+}
+
+nvoip_create_access_token() {
+  nvoip_require_var NVOIP_NUMBERSIP || return 1
+  nvoip_require_var NVOIP_USER_TOKEN || return 1
+  basic_auth="$(nvoip_resolve_basic_auth)" || return 1
+
+  curl -sS \
+    --request POST \
+    --header "Authorization: Basic $basic_auth" \
+    --header "Content-Type: application/x-www-form-urlencoded" \
+    --data "username=$NVOIP_NUMBERSIP&password=$NVOIP_USER_TOKEN&grant_type=password" \
+    "$NVOIP_BASE_URL/oauth/token"
+}
